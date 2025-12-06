1c5177799f02c379056fffdbeaaa0441d21d360c
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-06 21:01:21 +0300

    Add sprintSpeed on SHIFT

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,18 +9,40 @@ namespace Player
     {
         [SerializeField] private CharacterController m_characterController;
         [SerializeField] private CinemachineCamera m_cinemachineCamera;
-        [SerializeField] private float m_speed = 10f;
+        [SerializeField] private float m_currentSpeed;
+        [SerializeField] private float m_walkSpeed = 5f;
+        [SerializeField] private float m_sprintSpeed = 10f;
 
         private Vector2 m_move;
+
+        private void Start()
+        {
+            m_currentSpeed = m_walkSpeed;
+            
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
         
         public void OnMove(InputValue inputValue)
         {
             m_move =  inputValue.Get<Vector2>();
         }
+        
+        public void OnSprint(InputValue inputValue)
+        {
+            if (inputValue.Get<float>() > 0.5f)
+            {
+                m_currentSpeed =  m_sprintSpeed;
+            }
+            else
+            {
+                m_currentSpeed =  m_walkSpeed;
+            }
+        }
 
         private void Update()
         {
-            m_characterController.Move((GetForward() * m_move.y + GetRight() * m_move.x) * Time.deltaTime * m_speed);
+            m_characterController.Move((GetForward() * m_move.y + GetRight() * m_move.x) * Time.deltaTime * m_currentSpeed);
         }
 
         private Vector3 GetForward()
