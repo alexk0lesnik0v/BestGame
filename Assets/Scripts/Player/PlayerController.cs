00307bee6f39fbe8c01d3b0435df2bcb7ebec2f9
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-06 19:39:18 +0300

    Add new InputSystem

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private CharacterController m_characterController;
+        [SerializeField] private CinemachineCamera m_cinemachineCamera;
+        [SerializeField] private float m_speed = 10f;
+
+        private Vector2 m_move;
+        
+        public void OnMove(InputValue inputValue)
+        {
+            m_move =  inputValue.Get<Vector2>();
+        }
+
+        private void Update()
+        {
+            m_characterController.Move((GetForward() * m_move.y + GetRight() * m_move.x) * Time.deltaTime * m_speed);
+        }
+
+        private Vector3 GetForward()
+        {
+            Vector3 forward = m_cinemachineCamera.transform.forward;
+            forward.y = 0;
+            
+            return forward.normalized;
+        }
+        
+        private Vector3 GetRight()
+        {
+            Vector3 right = m_cinemachineCamera.transform.right;
+            right.y = 0;
+            
+            return right.normalized;
+        }
+    }
+}
