54d95345a43ec2bbdf84cebaa8f518ed575e61b3
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-07 22:02:10 +0300

    Add NavMesh for Enemies

---
+++ Assets/Scripts/Enemies/EnemyMonsterController.cs
@@ -0,0 +1,39 @@
+﻿using Player;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemies
+{
+    public class EnemyMonsterController:  MonoBehaviour
+
+    {
+        [SerializeField] private int m_health = 1;
+        
+        [SerializeField] private Character m_player;
+        private NavMeshAgent m_agent;
+      
+        void Start()
+        {
+            m_agent = GetComponent<NavMeshAgent>();
+        }
+        
+        void Update()
+        {
+            m_agent.SetDestination(m_player.transform.position);
+
+            if (m_health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+        /*public void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent<Stone>(out var stone))
+            {
+                m_health--;
+            }
+        }
+        */
+    }
+}
