ad28d364a53ee29c3ea106e944f3a113149ef94b
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-12 14:05:49 +0300

    Add Grab PickUpObject

---
+++ Assets/Scripts/PickUps/ObjectGrabbable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace PickUps
+{
+    public class ObjectGrabbable : MonoBehaviour
+    {
+        private Rigidbody m_objectRigidBody;
+        private Transform m_objectGrabPointTransform;
+
+        private void Awake()
+        {
+            m_objectRigidBody = GetComponent<Rigidbody>();
+        }
+        public void Grab(Transform objectGrabPointTransform)
+        {
+            this.m_objectGrabPointTransform = objectGrabPointTransform;
+            m_objectRigidBody.useGravity = false;
+        }
+
+        private void Update()  
+        {
+            if (m_objectGrabPointTransform is not null)
+            {
+                float m_lerpSpeed = 10f;
+                Vector3 newPosition = Vector3.Lerp(transform.position, m_objectGrabPointTransform.position, Time.deltaTime * m_lerpSpeed);
+                m_objectRigidBody.MovePosition(newPosition);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Players/PlayerDrop.cs
@@ -0,0 +1,31 @@
+﻿using PickUps;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Players
+{
+    public class PlayerDrop : MonoBehaviour
+    {
+        [SerializeField] private Transform m_playerCameraTransform;
+        [SerializeField] private Transform m_objectGrabPointTransform;
+        [SerializeField] private float m_pickUpDistance = 2f;
+        [SerializeField] private LayerMask m_pickUpLayerMask;
+
+        private void Update()
+        {
+            
+        }
+        
+        public void OnInteract(InputValue inputValue)
+        {
+           if (Physics.Raycast(m_playerCameraTransform.position, m_playerCameraTransform.forward,
+                   out RaycastHit hit, m_pickUpDistance, m_pickUpLayerMask))
+           {
+               if (hit.transform.TryGetComponent(out ObjectGrabbable objectGrabbable))
+               {
+                   objectGrabbable.Grab(m_objectGrabPointTransform);
+               }
+            }
+        }
+    }
+}
