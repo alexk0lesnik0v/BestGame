b1e064e7c76226afe836efa6c12040b657ff1b0b
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-25 22:24:52 +0300

    Fixed sound problem

--- Assets/Scripts/Interaction/DoorInteraction.cs
+++ Assets/Scripts/Interaction/DoorInteraction.cs
@@ -5,7 +5,12 @@ public class DoorInteraction : MonoBehaviour, IInteractable
 {
     [SerializeField] private Vector3 m_openDoorDown;
     [SerializeField] private Vector3 m_openDoorUp;
-    [SerializeField] private float m_duration = 2f;
+    [SerializeField] private float m_duration = 2.5f;
+    [SerializeField] private AudioClip m_soundDuration;
+    [SerializeField] private AudioSource m_audioSource;
+    
+    private bool m_isOpen = false;
+    
     public bool CanInteract()
     {
         return false;
@@ -17,10 +22,30 @@ public class DoorInteraction : MonoBehaviour, IInteractable
     }
     public void OpenDoorDown()
     {
-        transform.DOLocalMove(m_openDoorDown, m_duration);
+        if (!m_isOpen)
+        {
+            m_isOpen = false;
+        }
+        else
+        {
+            m_audioSource.PlayOneShot(m_soundDuration);
+            transform.DOLocalMove(m_openDoorDown, m_duration);
+            m_isOpen = false;
+        }
+        
+        
     }
     public void OpenDoorUp()
     {
-        transform.DOLocalMove(m_openDoorUp, m_duration);
+        if (m_isOpen)
+        {
+            m_isOpen = true;
+        }
+        else
+        {
+            m_audioSource.PlayOneShot(m_soundDuration);
+            transform.DOLocalMove(m_openDoorUp, m_duration);
+            m_isOpen = true;
+        }
     }
 }
