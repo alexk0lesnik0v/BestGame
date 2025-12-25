7c117132ebbc5f97352be0ae502f2afa570fa1c5
Oleg Polishchuk <o.polishchuk@xlab-game.com>
2025-12-26 02:22:18 +0300

    FixTimer

--- Assets/Scripts/Interaction/BtnInteraction.cs
+++ Assets/Scripts/Interaction/BtnInteraction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class BtnInteraction : MonoBehaviour, IInteractable
 {
     [SerializeField] private DoorInteraction m_doorInteraction;
+    [SerializeField] private Timer m_timer;
     private bool m_isOpen = false;
     public bool CanInteract()
     {
@@ -12,12 +13,14 @@ public class BtnInteraction : MonoBehaviour, IInteractable
 
     public bool Interact(Interactor interactor)
     {
+        m_timer.RestartTimer();
         if (m_isOpen)
         {
             m_doorInteraction.OpenDoorDown();
         }
         else
         {
+            m_timer.StartTimer();
             m_doorInteraction.OpenDoorUp();
         }
         m_isOpen = !m_isOpen;
--- Assets/Scripts/Timer/Timer.cs
+++ Assets/Scripts/Timer/Timer.cs
@@ -4,11 +4,42 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private TMP_Text m_timerText;
-    [SerializeField] private float m_time = 300;
+    [SerializeField] private float m_time = 301;
 
+    private float m_restartTime;
+    private bool m_isStart = false;
+    private bool m_canStart = false;
+    private void Start()
+    {
+        m_restartTime = m_time;
+    }
     void Update()
     {
-        m_time -= Time.deltaTime;
-        m_timerText.text = ((int)(m_time/60)).ToString() + " : " + ((int)(m_time % 60)).ToString();
+        if (m_isStart)
+        {
+            m_time -= Time.deltaTime;
+            m_timerText.text = ((int)(m_time / 60)).ToString() + " : " + ((int)(m_time % 60)).ToString();
+
+        }
+        else if(m_canStart)
+        {
+            
+            m_time -= Time.deltaTime;
+            m_timerText.text = ((int)(m_time / 60)).ToString() + " : " + ((int)(m_time % 60)).ToString();
+        }
+        else
+        {
+            m_time = m_restartTime;
+        }
+    }
+    public void StartTimer()
+    {
+        m_isStart = true;
+        m_canStart = true;
+    }
+    public void RestartTimer()
+    {
+        m_isStart = false;
+        m_time = m_restartTime;
     }
 }
