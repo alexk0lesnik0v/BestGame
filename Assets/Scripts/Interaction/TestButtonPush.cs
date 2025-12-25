5b0aab417cd4ac5c8c084633016bc760327fc096
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-25 04:26:29 +0300

    Add easy quest 1 win controller

--- Assets/Scripts/Interaction/TestButtonPush.cs
+++ Assets/Scripts/Interaction/TestButtonPush.cs
@@ -4,19 +4,26 @@ namespace Interaction
 {
     public class TestButtonPush : MonoBehaviour
     {
+        [SerializeField] private GameObject m_doorLight;
+        
         private Vector3 m_startTransform;
         private float m_positionY;
 
         private void Start()
         {
             m_startTransform = this.gameObject.transform.position;
+            m_doorLight.SetActive(true);
         }
         private void Update()
         {
             m_positionY = m_startTransform.y - this.transform.position.y;
             if (m_positionY >= 0.07)
             {
-                Debug.Log("Нажата");
+                m_doorLight.SetActive(false);
+            }
+            else 
+            {
+                m_doorLight.SetActive(true);
             }
         }
     }
---
+++ Assets/Scripts/QuestControllers/QuestControllerOne.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace QuestControllers
+{
+    public class QuestControllerOne : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_doorLight1;
+        [SerializeField] private GameObject m_doorLight2;
+        [SerializeField] private GameObject m_doorLight3;
+        [SerializeField] private GameObject m_doorLight4;
+        [SerializeField] private DoorInteraction m_doorInteraction;
+
+        private void Update()
+        {
+            if (!m_doorLight1.activeSelf && !m_doorLight2.activeSelf && !m_doorLight3.activeSelf &&
+                !m_doorLight4.activeSelf)
+            {
+                m_doorInteraction.OpenDoorUp();
+            }
+            else
+            {
+                m_doorInteraction.OpenDoorDown();
+            }
+        }
+    }
+}
