c05b3ad754d6e776903f08d0083cdc0f528e503c
alexk0lesnik0v <alexander74k@mail.ru>
2025-12-25 04:46:17 +0300

    create quest 1 win controller

--- Assets/Scripts/Interaction/OnePlatformController.cs
+++ Assets/Scripts/Interaction/OnePlatformController.cs
@@ -2,10 +2,9 @@
 
 namespace Interaction
 {
-    public class TestButtonPush : MonoBehaviour
+    public class OnePlatformController : MonoBehaviour
     {
-        [SerializeField] private GameObject m_doorLight1;
-        [SerializeField] private GameObject m_doorLight2;
+        [SerializeField] private GameObject m_doorLight;
         
         private Vector3 m_startTransform;
         private float m_positionY;
@@ -13,21 +12,18 @@ namespace Interaction
         private void Start()
         {
             m_startTransform = this.gameObject.transform.position;
-            m_doorLight1.SetActive(true);
+            m_doorLight.SetActive(true);
         }
         private void Update()
         {
             m_positionY = m_startTransform.y - this.transform.position.y;
             if (m_positionY >= 0.07)
             {
-                if(!m_doorLight2.activeSelf)
-                m_doorLight1.SetActive(false);
-                else 
-                    m_doorLight1.SetActive(true);
+                m_doorLight.SetActive(false);
             }
             else 
             {
-                m_doorLight1.SetActive(true);
+                m_doorLight.SetActive(true);
             }
         }
     }
