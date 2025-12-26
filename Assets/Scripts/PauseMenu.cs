278ce7fd7318078f218c45edb68011b4dbf6c835
Oleg Polishuk <o.polishchuk@xlab-game.com>
2025-12-26 15:50:34 +0300

    AddPauseMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject m_pauseMenu;
+    [SerializeField] private Button m_resumeButton;
+    [SerializeField] private Button m_pauseButton;
+    
+    private bool m_isPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnPause();
+        }
+        else
+        {
+            OnResume();
+        }
+    }
+    public void OnPause()
+    {
+        m_pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        m_isPaused = true;
+    }
+    public void OnResume()
+    {
+        m_pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        m_isPaused = false;
+    }
+    private void OnEnable()
+    {
+        m_resumeButton.onClick.AddListener(OnResume);
+        m_pauseButton.onClick.AddListener(OnPause);
+    }
+    private void OnDisable()
+    {
+        m_resumeButton.onClick?.RemoveListener(OnResume);
+        m_pauseButton.onClick?.RemoveListener(OnPause);
+    }
+}
