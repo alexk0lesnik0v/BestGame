dbac16b728cae65fd705d793005e6f6d9430ca1f
alexk0lesnik0v <alexander74k@mail.ru>
2026-01-19 21:51:20 +0300

    Add Inventory Canvas

---
+++ Assets/Scripts/Inventories/ItemScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Inventories
+{
+    public class ItemScriptableObject : ScriptableObject
+    {
+        [SerializeField] private string m_itemName;
+        [SerializeField] private int m_maximumAmount;
+        [SerializeField] private string m_itemDescription;
+    }
+}
