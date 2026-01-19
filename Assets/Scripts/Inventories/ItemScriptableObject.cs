75e86567e1a80d3033ae80bf8d4aa3f286ef0e5a
alexk0lesnik0v <alexander74k@mail.ru>
2026-01-19 22:02:37 +0300

    Add Food Item

--- Assets/Scripts/Inventories/ItemScriptableObject.cs
+++ Assets/Scripts/Inventories/ItemScriptableObject.cs
@@ -2,8 +2,10 @@
 
 namespace Inventories
 {
+    public enum ItemType {Default, Food, Weapon, Instrument}
     public class ItemScriptableObject : ScriptableObject
     {
+        public ItemType m_itemType;
         [SerializeField] private string m_itemName;
         [SerializeField] private int m_maximumAmount;
         [SerializeField] private string m_itemDescription;
