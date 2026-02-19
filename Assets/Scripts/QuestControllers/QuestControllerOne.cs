4eb8d45f761c10aa49d5b154a40dc6a2a81fd587
alexk0lesnik0v <alexander74k@mail.ru>
2026-02-20 02:13:59 +0300

    Fixed mechanics of quest 1

--- Assets/Scripts/QuestControllers/QuestControllerOne.cs
+++ Assets/Scripts/QuestControllers/QuestControllerOne.cs
@@ -25,6 +25,14 @@ namespace QuestControllers
         [SerializeField] private DoorInteraction m_doorQuestInteraction;
         [SerializeField] private DoorInteraction m_doorInteraction;
         
+        [SerializeField] private AudioSource m_audioSource;
+        [SerializeField] private AudioClip m_audioClip;
+
+        private bool m_isOne = false;
+        private bool m_isTue = false;
+        private bool m_isThree = false;
+        private bool m_isFour = false;
+        
         private int m_winIndex = 0;
         
         private void Start()
@@ -47,44 +55,83 @@ namespace QuestControllers
                 m_winIndex = 0;
             }
             
-            if (m_doorLight1.activeSelf && m_doorLight2.activeSelf && m_doorLight3.activeSelf &&
-                m_doorLight4.activeSelf)
-            {
-                m_winIndex = 0;
-            }
-
             if (m_button1.m_isPushed && !m_button2.m_isPushed && m_button3.m_isPushed)
             {
-                m_winIndex = 0;
+                m_winIndex = 1;
             }
 
             if (m_button1.m_isPushed && !m_button3.m_isPushed && m_button4.m_isPushed)
             {
-                m_winIndex = 0;
+                if (m_winIndex == 2)
+                {
+                    m_winIndex = 2;
+                }
+                else if (m_winIndex == 1)
+                {
+                    m_winIndex = 1;
+                }
+            }
+
+            if (m_winIndex == 2 && !m_button2.m_isPushed)
+            {
+                m_winIndex = 1;
+                m_isTue = false;
+                m_doorLight2.SetActive(true);
+                m_doorGreenLight2.SetActive(false);
+            }
+            
+            if (m_winIndex == 3 && !m_button3.m_isPushed)
+            {
+                m_winIndex = 2;
+                m_isThree = false;
             }
             
             if (m_button1.m_isPushed && !m_button2.m_isPushed && !m_button3.m_isPushed &&
                 !m_button4.m_isPushed && m_winIndex == 0)
             {
                 m_winIndex = 1;
+                
+                if (!m_isOne)
+                {
+                    m_audioSource.PlayOneShot(m_audioClip);
+                    m_isOne = true;
+                }
             }
             
             if (m_button1.m_isPushed && m_button2.m_isPushed && !m_button3.m_isPushed &&
                 !m_button4.m_isPushed && m_winIndex == 1)
             {
                 m_winIndex = 2;
+                
+                if (!m_isTue)
+                {
+                    m_audioSource.PlayOneShot(m_audioClip);
+                    m_isTue = true;
+                }
             }
             
             if (m_button1.m_isPushed && m_button2.m_isPushed && m_button3.m_isPushed &&
                 !m_button4.m_isPushed && m_winIndex == 2)
             {
                 m_winIndex = 3;
+                
+                if (!m_isThree)
+                {
+                    m_audioSource.PlayOneShot(m_audioClip);
+                    m_isThree = true;
+                }
             }
             
             if (m_button1.m_isPushed && m_button2.m_isPushed && m_button3.m_isPushed &&
                 m_button4.m_isPushed && m_winIndex == 3)
             {
                 m_winIndex = 4;
+                
+                if (!m_isFour)
+                {
+                    m_audioSource.PlayOneShot(m_audioClip);
+                    m_isFour = true;
+                }
             }
             
             if (m_winIndex ==4)
@@ -132,6 +179,11 @@ namespace QuestControllers
                 m_doorGreenLight2.SetActive(false);
                 m_doorGreenLight3.SetActive(false);
                 m_doorGreenLight4.SetActive(false);
+                
+                m_isOne = false;
+                m_isTue = false;
+                m_isThree = false;
+                m_isFour = false;
             }
         }
 
