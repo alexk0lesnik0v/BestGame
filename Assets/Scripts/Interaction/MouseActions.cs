c292a20ba82e742ce85f60ef033ecbcd4251e033
alexk0lesnik0v <alexander74k@mail.ru>
2026-02-20 03:59:56 +0300

    Add MouseActions

---
+++ Assets/Scripts/Interaction/MouseActions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Interaction
+{
+    public class MouseActions : MonoBehaviour
+    {
+        public UnityEvent m_enter;
+        public UnityEvent m_exit;
+        
+        private void OnMouseEnter()
+        {
+            m_enter?.Invoke();
+        }
+
+        private void OnMouseExit()
+        {
+            m_exit?.Invoke();
+        }
+    }
+}
