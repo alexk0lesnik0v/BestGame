eae5ca7cf13550783a6be4351fbbdeb7430bbb34
Oleg Polishchuk <o.polishchuk@xlab-game.com>
2026-02-27 13:10:56 +0300

    AddEnvironmentLab2

--- Assets/Scripts/WinController.cs
+++ Assets/Scripts/WinController.cs
@@ -5,7 +5,6 @@ namespace DefaultNamespace
 {
     public class WinController :  MonoBehaviour
     {
-        [SerializeField] private GameObject m_winUI;
         [SerializeField] private PlayerController m_player;
         [SerializeField] private AudioClip m_soundWin;
         [SerializeField] private AudioSource m_audioSource;
@@ -14,8 +13,7 @@ namespace DefaultNamespace
         {
             if (other.gameObject.TryGetComponent<Player>(out var player))
             {
-                m_winUI.SetActive(true);
-                m_audioSource.PlayOneShot(m_soundWin);
+                //m_audioSource.PlayOneShot(m_soundWin);
                 m_player.enabled = false;
                 m_player.m_isNotFiring = true;
                 
