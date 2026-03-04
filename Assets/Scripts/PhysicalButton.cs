d08758714962b236453cbd5e98cd5c72b11a40a4
Oleg Polishchuk <o.polishchuk@xlab-game.com>
2026-03-04 17:30:47 +0300

    Fix

--- Assets/Scripts/PhysicalButton.cs
+++ Assets/Scripts/PhysicalButton.cs
@@ -10,16 +10,18 @@ public class PhysicalButton : MonoBehaviour
     [SerializeField] private Transform m_movable;
     [SerializeField] private Vector3 m_targetLocalPosition;
 
+    private Tweener m_tweener;
     private bool m_isAnimating;
 
     private void OnMouseUpAsButton()
     {
+        m_tweener.Kill();
         if (m_isAnimating)
         {
             return;
         }
         m_isAnimating = true;
-        m_movable.DOLocalMove(m_targetLocalPosition, m_animationDuration / 2).OnComplete(OnComplete);
+        m_tweener = m_movable.DOLocalMove(m_targetLocalPosition, m_animationDuration / 2).OnComplete(OnComplete);
     }
 
     private void OnComplete()
--- Assets/Scripts/States/GameplayState.cs
+++ Assets/Scripts/States/GameplayState.cs
@@ -1,22 +1,42 @@
 using Players;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameplayState : StateBase
 {
     [SerializeField] private Player m_player;
 
     private GameStateMachine m_gameStateMachine;
+    private InputSystem_Actions m_gameInput;
+
+    private void Awake()
+    {
+        m_gameInput = new InputSystem_Actions();
+        m_gameInput.Enable();
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            m_gameStateMachine.Enter<PauseState>();
-        }
         if (m_player.health <= 0)
         {
             m_gameStateMachine.Enter<DeadState>();
         }
+    }
+
+    private void OnEnable()
+    {
+        m_gameInput.Player.Escape.performed += OnEscape;
+    }
+
+    private void OnDisable()
+    {
+        m_gameInput.Player.Escape.performed -= OnEscape;
+    }
+
+    private void OnEscape(InputAction.CallbackContext context)
+    {
+        m_gameStateMachine.Enter<PauseState>();
     }
 
     public override void Initialize(GameStateMachine gameStateMachine)
