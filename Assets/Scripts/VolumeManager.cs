47fe6cb0ed80375247d4c745be6e74135fe8cf2b
Oleg Polishchuk <o.polishchuk@xlab-game.com>
2026-03-10 01:31:32 +0300

    Fix

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -8,40 +8,40 @@ public class VolumeManager : MonoBehaviour
     public AudioMixer audioMixer;
     public string volumeParameter = "MasterVolume";
     public float volumeStep = 0.1f;
-    private float currentVolume = 1.0f;
+    private float m_currentVolume = 1.0f;
 
     void Start()
     {
-        currentVolume = PlayerPrefs.GetFloat("LinearVolume", 1.0f);
-        float decibelVolume = Mathf.Log10(currentVolume) * 20;
+        m_currentVolume = PlayerPrefs.GetFloat("LinearVolume", 1.0f);
+        float decibelVolume = Mathf.Log10(m_currentVolume) * 20;
         audioMixer.SetFloat(volumeParameter, decibelVolume);
         UpdateVolumeText();
     }
 
     public void DecreaseVolume()
     {
-        currentVolume -= volumeStep;
-        if (currentVolume < 0)
-            currentVolume = 0;
-        float decibelVolume = Mathf.Log10(currentVolume) * 20;
+        m_currentVolume -= volumeStep;
+        if (m_currentVolume < 0)
+            m_currentVolume = 0;
+        float decibelVolume = Mathf.Log10(m_currentVolume) * 20;
         audioMixer.SetFloat(volumeParameter, decibelVolume);
         UpdateVolumeText();
     }
 
     public void IncreaseVolume()
     {
-        currentVolume += volumeStep;
-        if (currentVolume > 1)
-            currentVolume = 1;
-        float decibelVolume = Mathf.Log10(currentVolume) * 20;
+        m_currentVolume += volumeStep;
+        if (m_currentVolume > 1)
+            m_currentVolume = 1;
+        float decibelVolume = Mathf.Log10(m_currentVolume) * 20;
         audioMixer.SetFloat(volumeParameter, decibelVolume);
         UpdateVolumeText();
     }
 
     private void UpdateVolumeText()
     {
-        int volumePercentage = Mathf.RoundToInt(currentVolume * 100);
+        int volumePercentage = Mathf.RoundToInt(m_currentVolume * 100);
         volumeText.text = $"Громкость: {volumePercentage}%";
-        PlayerPrefs.SetFloat("LinearVolume", currentVolume);
+        PlayerPrefs.SetFloat("LinearVolume", m_currentVolume);
     }
 }
